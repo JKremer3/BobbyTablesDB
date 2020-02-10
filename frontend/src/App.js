0f1a9a44ed8d38c47d0f1736166ff8c6a23cc79e
Peter Brennan <peter.brennan@wsu.edu>
2020-02-10 09:34:05 -0800

    Made the app component a class component so we can use state :ambulance:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,51 +4,61 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from 'react-bootstrap/Table';
 import './App.css';
 
-function App() {
-  return (
-      <div className="App">
-          <div >
-              <select id="State">
-                  <option value="WA">WA</option>
-                  <option value="ID">ID</option>
-                  <option value="FL">FL</option>
-                  <option value="CA">CA</option>
-              </select>
-              <select id="City">
-                  <option value="WA">Pullman</option>
-                  <option value="ID">Vancouver</option>
-                  <option value="FL">Seattle</option>
-                  <option value="CA">Spokane</option>
-              </select>
-          </div>
-          <Table striped bordered hover>
-              <thead>
-                  <tr>
-                      <th>Business Name</th>
-                      <th>State</th>
-                      <th>City</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td>1</td>
-                      <td>Mark</td>
-                      <td>Otto</td>
-                  </tr>
-                  <tr>
-                      <td>2</td>
-                      <td>Jacob</td>
-                      <td>Thornton</td>
-                  </tr>
-                  <tr>
-                      <td colSpan="2">Larry the Bird</td>
-                      <td>@twitter</td>
-                  </tr>
-              </tbody>
-          </Table>
-          <div></div>
-      </div>
-  );
+class App extends React.Component {
+    constructor(){
+        
+    }
+
+    toggleModal(){
+         
+    }
+
+    render(){
+        return (
+            <div className="App">
+                <div >
+                    <select id="State">
+                        <option value="WA">WA</option>
+                        <option value="ID">ID</option>
+                        <option value="FL">FL</option>
+                        <option value="CA">CA</option>
+                    </select>
+                    <select id="City">
+                        <option value="WA">Pullman</option>
+                        <option value="ID">Vancouver</option>
+                        <option value="FL">Seattle</option>
+                        <option value="CA">Spokane</option>
+                    </select>
+                </div>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Business Name</th>
+                            <th>State</th>
+                            <th>City</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </Table>
+                <div></div>
+            </div>
+        );
+    }
 }
 
 export default App;
