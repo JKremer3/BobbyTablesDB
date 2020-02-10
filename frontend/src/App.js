e5f716a22180fd3bdbe3d16bfddcee964e487661
Peter Brennan <peter.brennan@wsu.edu>
2020-02-10 09:11:15 -0800

    Installed react-table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,20 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-          <select id="State">
-              <option value="WA">WA</option>
-              <option value="ID">ID</option>
-              <option value="FL">FL</option>
-              <option value="CA">CA</option>
-          </select>
-          <select id="City">
-              <option value="WA">Pullman</option>
-              <option value="ID">Vancouver</option>
-              <option value="FL">Seattle</option>
-              <option value="CA">Spokane</option>
-          </select>
+          <div >
+              <select id="State">
+                  <option value="WA">WA</option>
+                  <option value="ID">ID</option>
+                  <option value="FL">FL</option>
+                  <option value="CA">CA</option>
+              </select>
+              <select id="City">
+                  <option value="WA">Pullman</option>
+                  <option value="ID">Vancouver</option>
+                  <option value="FL">Seattle</option>
+                  <option value="CA">Spokane</option>
+              </select>
+          </div>
       <div></div>
       <div></div>
   </div>
