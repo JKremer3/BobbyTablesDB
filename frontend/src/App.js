5480fb3b370a531b9aff7f4ed6a8f10d25ac4685
Peter Brennan <peter.brennan@wsu.edu>
2020-02-10 09:29:10 -0800

    Renamed column headers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,10 +24,9 @@ function App() {
           <Table striped bordered hover>
               <thead>
                   <tr>
-                      <th>#</th>
-                      <th>First Name</th>
-                      <th>Last Name</th>
-                      <th>Username</th>
+                      <th>Business Name</th>
+                      <th>State</th>
+                      <th>City</th>
                   </tr>
               </thead>
               <tbody>
@@ -35,16 +34,13 @@ function App() {
                       <td>1</td>
                       <td>Mark</td>
                       <td>Otto</td>
-                      <td>@mdo</td>
                   </tr>
                   <tr>
                       <td>2</td>
                       <td>Jacob</td>
                       <td>Thornton</td>
-                      <td>@fat</td>
                   </tr>
                   <tr>
-                      <td>3</td>
                       <td colSpan="2">Larry the Bird</td>
                       <td>@twitter</td>
                   </tr>
