994718debf4502d0c2d54e9c3ae7155da37a4088
Lance Hoffmann <neprotalis@gmail.com>
2020-03-24 16:57:43 -0700

    updated schema

--- DBschema/BobbyTables_schema.sql
+++ DBschema/BobbyTables_schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Business(
     --Non-Composite Attributes
-    busId NVARCHAR(25) NOT NULL,
-    busName NVARCHAR(100) NOT NULL,
-    city NVARCHAR(30) NOT NULL,
+    busId VARCHAR(25) NOT NULL,
+    busName VARCHAR(100) NOT NULL,
+    city VARCHAR(30) NOT NULL,
     busState CHAR(2) NOT NULL,
     postalCode CHAR(5) NOT NULL,
     lat numeric(9, 6),
@@ -17,23 +17,23 @@ CREATE TABLE Business(
 
 CREATE TABLE BusCategory(
     --This table holds all categories associated with busness
-    busId NVARCHAR(25) NOT NULL,
-    category NVARCHAR(25),
+    busId VARCHAR(25) NOT NULL,
+    category VARCHAR(25),
     PRIMARY KEY(busId,category),
     FOREIGN KEY(busId) REFERENCES Business(busId)
 );
 
 CREATE TABLE BusAttributes(
-    busId NVARCHAR(25) NOT NULL,
-    attributeName NVARCHAR(20) NOT NULL,
-    attributeVal NVARCHAR(30) NOT NULL,
+    busId VARCHAR(25) NOT NULL,
+    attributeName VARCHAR(20) NOT NULL,
+    attributeVal VARCHAR(30) NOT NULL,
     PRIMARY KEY(busId, attributeName),
     FOREIGN KEY(busId) REFERENCES Business(busId)
 );
 
 CREATE TABLE BusGoodForMeals(
-    busId NVARCHAR(25) NOT NULL,
-    mealType NVARCHAR(10) NOT NULL,
+    busId VARCHAR(25) NOT NULL,
+    mealType VARCHAR(10) NOT NULL,
     mealVal BOOLEAN,
     
     PRIMARY KEY(busId, mealType),
@@ -41,8 +41,8 @@ CREATE TABLE BusGoodForMeals(
 );
 
 CREATE TABLE BusAmbience(
-    busId NVARCHAR(25) NOT NULL,
-    ambienceType NVARCHAR(9) NOT NULL,
+    busId VARCHAR(25) NOT NULL,
+    ambienceType VARCHAR(9) NOT NULL,
     ambienceVal BOOLEAN,
 
     PRIMARY KEY(busId, ambienceType),
@@ -50,8 +50,8 @@ CREATE TABLE BusAmbience(
 );
 
 CREATE TABLE BusParking(
-    busId NVARCHAR(25) NOT NULL,
-    parkingType NVARCHAR(15) NOT NULL,
+    busId VARCHAR(25) NOT NULL,
+    parkingType VARCHAR(15) NOT NULL,
     parkVal BOOLEAN,
     
     PRIMARY KEY(busId, parkingType),
@@ -59,17 +59,17 @@ CREATE TABLE BusParking(
 );
 
 CREATE TABLE BusHours(
-    busId NVARCHAR(25) NOT NULL,
-    dayOfWeek  NVARCHAR(9) NOT NULL,
-    hrOpen NVARCHAR(5),
-    hrClosed VARCHAR(5)
+    busId VARCHAR(25) NOT NULL,
+    dayOfWeek  VARCHAR(9) NOT NULL,
+    hrOpen VARCHAR(5),
+    hrClosed VARCHAR(5),
     
     
     PRIMARY KEY(busId, dayOfWeek),
     FOREIGN KEY(busId) REFERENCES Business(busId)
 );
 
-CREATE TABLE User(
+CREATE TABLE Users(
     avgStars REAL,
     cool INT NOT NULL,
     funny INT NOT NULL,
@@ -77,10 +77,10 @@ CREATE TABLE User(
     fans INT,
     userLat CHAR(13),
     userLong CHAR(13),
-    userName NVARCHAR(20) NOT NULL,
+    userName VARCHAR(20) NOT NULL,
     tipCount INT NOT NULL,
     useful INT NOT NULL,
-    userId NVARCHAR(25) NOT NULL,
+    userId VARCHAR(25) NOT NULL,
     yelpStartDate CHAR(10) NOT NULL,
     yelpStartTime CHAR(8) NOT NULL,
     lat numeric(9, 6),
@@ -91,35 +91,35 @@ CREATE TABLE User(
 CREATE TABLE Tip(
     --table for the tip weak entity
     --tip requires both a valid user and busness ID
-    busId NVARCHAR(25) NOT NULL,
-    userId NVARCHAR(25) NOT NULL,
+    busId VARCHAR(25) NOT NULL,
+    userId VARCHAR(25) NOT NULL,
     likeCount INT,
-    tipText NVARCHAR(150) NOT NULL,
+    tipText VARCHAR(150) NOT NULL,
     tipDate CHAR(10) NOT NULL,
     tipTime CHAR(8) NOT NULL,
     PRIMARY KEY (busId, userId, tipDate),
-    FOREIGN KEY busId REFERENCES Business(busId),
-    FOREIGN Key userId REFERENCES User(userId)
+    FOREIGN KEY (busId) REFERENCES Business(busId),
+    FOREIGN Key (userId) REFERENCES Users(userId)
 );
 
 CREATE TABLE Friends(
     --Table for the friends relation
     --userId and friendId must exist in User.userId
-    userId NVARCHAR(25) NOT NULL,
-    friendId NVARCHAR(25) NOT NULL,
+    userId VARCHAR(25) NOT NULL,
+    friendId VARCHAR(25) NOT NULL,
     PRIMARY KEY (userId, friendId),
-    FOREIGN KEY userId REFERENCES User(userId),
-    FOREIGN KEY friendId REFERENCES User(userId)
+    FOREIGN KEY (userId) REFERENCES Users(userId),
+    FOREIGN KEY (friendId) REFERENCES Users(userId)
 );
 
 CREATE TABLE checkin(
     --Table for the check-in weak entity
     --a checkin must be associated with a valid busness ID
-    busId NVARCHAR(25) NOT NULL,
+    busId VARCHAR(25) NOT NULL,
     checkYear CHAR(4) NOT NULL,
     checkDate CHAR(10) NOT NULL,
     checkMonth CHAR(2) NOT NULL,
     checkTime CHAR(8) NOT NULL,
     PRIMARY KEY (busId, checkDate, checkMonth, checkYear, checkTime),
-    FOREIGN KEY busId REFERENCES Business(busId)
+    FOREIGN KEY (busId) REFERENCES Business(busId)
 );
