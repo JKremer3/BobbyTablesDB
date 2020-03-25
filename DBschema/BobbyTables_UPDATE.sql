648e567c902e7196f0b8f8853f9e7a91db867cca
Jeff <jeffrey.kremer.iii@gmail.com>
2020-03-24 17:36:59 -0700

    Create Update Statement for Business.numTips

--- DBschema/BobbyTables_UPDATE.sql
+++ DBschema/BobbyTables_UPDATE.sql
@@ -1,3 +1,7 @@
 UPDATE Business, (select busId, count(*) as checkCount from checkin group by busId) as checkTbl
 SET Business.numCheckins = checkTbl.checkCount
 WHERE Business.busId = checkTbl.busId;
+
+UPDATE Business, (select busId, count(*) as tipCount from Tip group by busId) as tipTbl
+SET Business.numTips = tipTbl.tipCount
+WHERE Business.busId = tipTbl.busId;
