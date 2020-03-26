dea41391086bafdb4df8ef19c32563bf3a12e31b
Jeff <jeffrey.kremer.iii@gmail.com>
2020-03-26 13:30:54 -0700

    actually create trigger for updating Business.numCheckins

--- DBschema/BobbyTables_TRIGGER.sql
+++ DBschema/BobbyTables_TRIGGER.sql
@@ -1,5 +1,15 @@
-create function update_business_checkins() RETURNS trigger as $update_business_checkins$ 
+create or replace function update_business_checkins() 
+    RETURNS trigger 
+as $update_business_checkins$ 
     BEGIN
-    --
-    --update business.numCheckins()
-    --
+        UPDATE Business, (select busId, count(*) as checkCount from checkin group by busId) as checkTbl
+            SET Business.numCheckins = checkTbl.checkCount
+            WHERE Business.busId = checkTbl.busId;
+        RETURN NEW;
+$update_business_checkins$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER UpdateBusCheckins
+AFTER INSERT OR DELETE ON checkin
+FOR EACH ROW
+EXECUTE PROCEDURE update_business_checkins();  
