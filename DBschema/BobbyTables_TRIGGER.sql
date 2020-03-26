6389fbea7fa461abb3c278b6a6c4aa160de2bfee
Jeff <jeffrey.kremer.iii@gmail.com>
2020-03-26 13:38:10 -0700

    create trigger for updating Business.numTips

--- DBschema/BobbyTables_TRIGGER.sql
+++ DBschema/BobbyTables_TRIGGER.sql
@@ -1,3 +1,4 @@
+--function to update business records with their checkin count
 create or replace function update_business_checkins() 
     RETURNS trigger 
 as $update_business_checkins$ 
@@ -9,7 +10,26 @@ as $update_business_checkins$
 $update_business_checkins$ 
 LANGUAGE plpgsql;
 
+--Trigger statement to call the update function
 CREATE TRIGGER UpdateBusCheckins
 AFTER INSERT OR DELETE ON checkin
 FOR EACH ROW
-EXECUTE PROCEDURE update_business_checkins();  
+EXECUTE PROCEDURE update_business_checkins();
+
+--function to update business records with their tip count
+create or replace function update_business_tips() 
+    RETURNS trigger 
+as $update_business_tips$ 
+    BEGIN
+        UPDATE Business, (select busId, count(*) as tipCount from Tip group by busId) as tipTbl
+            SET Business.numTips = tipTbl.tipCount
+            WHERE Business.busId = tipTbl.busId;
+        RETURN NEW;
+$update_business_tips$ 
+LANGUAGE plpgsql;
+
+--Trigger statement to call the update function
+CREATE TRIGGER UpdateBusTips
+AFTER INSERT OR DELETE ON Tip
+FOR EACH ROW
+EXECUTE PROCEDURE update_business_tips();  
