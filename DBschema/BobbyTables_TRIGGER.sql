1b66f6a630b86ef07401a31a84edd3fea90bcbd3
Jeff <jeffrey.kremer.iii@gmail.com>
2020-03-26 13:43:37 -0700

    create trigger for updating Users.tipCount

--- DBschema/BobbyTables_TRIGGER.sql
+++ DBschema/BobbyTables_TRIGGER.sql
@@ -10,7 +10,7 @@ as $update_business_checkins$
 $update_business_checkins$ 
 LANGUAGE plpgsql;
 
---Trigger statement to call the update function
+--Trigger statement to call update_business_checkins()
 CREATE TRIGGER UpdateBusCheckins
 AFTER INSERT OR DELETE ON checkin
 FOR EACH ROW
@@ -28,8 +28,26 @@ as $update_business_tips$
 $update_business_tips$ 
 LANGUAGE plpgsql;
 
---Trigger statement to call the update function
+--Trigger statement to call update_business_tips()
 CREATE TRIGGER UpdateBusTips
 AFTER INSERT OR DELETE ON Tip
 FOR EACH ROW
-EXECUTE PROCEDURE update_business_tips();  
+EXECUTE PROCEDURE update_business_tips();
+
+--function to update Users records with their tip count
+create or replace function update_users_tips() 
+    RETURNS trigger 
+as $update_users_tips$ 
+    BEGIN
+        UPDATE Users, (select userId, count(*) as tipCount from Tip group by userId) as tipTbl
+            SET Users.tipCount = tipTbl.tipCount
+            WHERE Users.userId = tipTbl.userId;
+        RETURN NEW;
+$update_users_tips$ 
+LANGUAGE plpgsql;
+
+--Trigger statement to call update_users_tips()
+CREATE TRIGGER UpdateUsersTips
+AFTER INSERT OR DELETE ON Tip
+FOR EACH ROW
+EXECUTE PROCEDURE update_users_tips();  
