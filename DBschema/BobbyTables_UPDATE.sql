8a2139fe2b4b9d746c66a7f456a3e4d50725075f
Jeff <jeffrey.kremer.iii@gmail.com>
2020-03-27 21:51:38 -0700

    make update statements more readable

--- DBschema/BobbyTables_UPDATE.sql
+++ DBschema/BobbyTables_UPDATE.sql
@@ -4,9 +4,20 @@ SET numCheckins = (select count(busId) from checkin WHERE Business.busId = check
 UPDATE Business 
 SET numTips = (select count(busId) from Tip WHERE Business.busId = Tip.busId);
 
-UPDATE Users SET tipCount = res.ccount FROM ( SELECT userId, COUNT(userId) ccount FROM Tip GROUP BY Tip.userId) res WHERE Users.userId = res.userId;
+UPDATE Users SET tipCount = res.ccount 
+FROM 
+( 
+    SELECT userId, COUNT(userId) ccount 
+    FROM Tip 
+    GROUP BY Tip.userId
+) res 
+WHERE Users.userId = res.userId;
 
-UPDATE Users SET totalLikes = res.likeSum FROM
-(SELECT Tip.userID, SUM(likeCount) likeSum
-FROM Tip
-GROUP BY userID) res WHERE Users.userId = res.userId;
+UPDATE Users SET totalLikes = res.likeSum 
+FROM
+(
+    SELECT Tip.userID, SUM(likeCount) likeSum
+    FROM Tip
+    GROUP BY userID
+) res 
+WHERE Users.userId = res.userId;
